3ee7369ccfa68b629082cd5087a8b717c88d2a44
kim hyukki <74032346+Sunflowerrrrr@users.noreply.github.com>
2024-08-02 14:49:57 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n; cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++) {
+            if(j<=n-i) printf(" ");
+            else printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
