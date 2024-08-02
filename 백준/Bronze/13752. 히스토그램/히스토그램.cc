39b4614a14fe881ae8bda927865a4121f0f23e0d
kim hyukki <74032346+Sunflowerrrrr@users.noreply.github.com>
2024-08-02 15:22:58 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    int n; cin>>n;
+    while(n--) {
+        int k; cin>>k;
+        while(k--) printf("=");
+        printf("\n");
+    }
+}
