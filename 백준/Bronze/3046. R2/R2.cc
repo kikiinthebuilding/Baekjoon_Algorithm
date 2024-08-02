29f16a47810aa068dad7411b3afb9b57e9604fa9
kim hyukki <74032346+Sunflowerrrrr@users.noreply.github.com>
2024-08-02 14:57:39 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int r1, s; cin>>r1>>s;
+    int r2 = s*2 -r1;
+    printf("%d", r2);
+    return 0;
+}
