37f2ac779c12b1d497f41da34eed6557290becd1
kim hyukki <74032346+kikiinthebuilding@users.noreply.github.com>
2024-09-13 16:04:26 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
+bool palindrome(string s) {
+    int len = s.length();
+    for(int i=0; i<len/2; i++) {
+        if(s[i]!=s[len-i-1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
 int main() {
     string s; bool sw;
     while((cin>>s) && s[0]!='0') {
-        sw = true;
-        int len = s.length();
-        for(int i=0; i<len/2; i++) {
-            if(s[i]!=s[len-i-1]) {
-                sw = false;
-                break;
-            }
-        }
-        if(sw) cout<<"yes\n";
-        if(!sw) cout<<"no\n";
+        if(palindrome(s)) cout<<"yes\n";
+        else cout<<"no\n";
     }
     return 0;
 }
