92b93e1e69782bfbd27a53aa9fc23d2079bede9b
kim hyukki <74032346+kikiinthebuilding@users.noreply.github.com>
2024-09-13 15:52:18 +0900

    [Bronze II] Title: 단어의 개수, Time: 52 ms, Memory: 5964 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -3,11 +3,10 @@
 using namespace std;
 
 int main() {
-    string s; getline(cin, s);
+    string s, s1; getline(cin, s);
     stringstream ss(s);
-    string s1; int cnt=0;
-    while (ss>>s1)
-        cnt++;
+    int cnt=0;
+    while (ss>>s1) cnt++;
     cout<<cnt<<'\n';
     return 0;
 }
